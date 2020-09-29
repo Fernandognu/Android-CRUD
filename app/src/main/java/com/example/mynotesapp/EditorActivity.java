23e595af237efd0d010e335ef374d2eb0da4bd84
Fernando Garcia <fernandogdac@gmail.com>
2020-09-29 14:57:37 -0300

    Update EditorActivity.java

--- app/src/main/java/com/example/mynotesapp/EditorActivity.java
+++ app/src/main/java/com/example/mynotesapp/EditorActivity.java
@@ -1,9 +1,10 @@
 package com.example.mynotesapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,16 +12,20 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import com.thebluealliance.spectrum.SpectrumPalette;
 
-public class EditorActivity extends AppCompatActivity {
+public class EditorActivity implements EditorView {
 
     EditText et_title, et_note;
     ProgressDialog progressDialog;
+    SpectrumPalette palette;
 
-    ApiInterface apiInterface;
+    EditorPresenter presenter;
+
+    int color, id;
+    String title, note;
+
+    Menu actionMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +34,26 @@ public class EditorActivity extends AppCompatActivity {
 
         et_title = findViewById(R.id.title);
         et_note = findViewById(R.id.note);
+        palette = findViewById(R.id.palette);
 
+        palette.setOnColorSelectedListener(
+                clr -> color = clr
+        );
+
+
+//      Progress Dialog
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Please wait...");
+
+        presenter = new EditorPresenter(this);
+
+        Intent intent = getIntent();
+        id = intent.getIntExtra("id", 0);
+        title = intent.getStringExtra("title");
+        note = intent.getStringExtra("note");
+        color = intent.getIntExtra("color", 0);
+
+        setDataFromIntentExtra();
 
     }
 
@@ -39,70 +61,142 @@ public class EditorActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_editor, menu);
+        actionMenu = menu;
+
+        if (id != 0) {
+            actionMenu.findItem(R.id.edit).setVisible(true);
+            actionMenu.findItem(R.id.delete).setVisible(true);
+            actionMenu.findItem(R.id.save).setVisible(false);
+            actionMenu.findItem(R.id.update).setVisible(false);
+        } else {
+            actionMenu.findItem(R.id.edit).setVisible(false);
+            actionMenu.findItem(R.id.delete).setVisible(false);
+            actionMenu.findItem(R.id.save).setVisible(true);
+            actionMenu.findItem(R.id.update).setVisible(false);
+        }
+
+
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
+        String title = et_title.getText().toString().trim();
+        String note = et_note.getText().toString().trim();
+        int color = this.color;
+
         switch (item.getItemId()) {
             case R.id.save:
                 //Save
+                if (title.isEmpty()) {
+                    et_title.setError("Please enter a title");
+                } else if (note.isEmpty()) {
+                    et_note.setError("Please enter a note");
+                } else {
+                    presenter.saveNote(title, note, color);
+                }
+                return true;
 
-                String title = et_title.getText().toString().trim();
-                String note = et_note.getText().toString().trim();
-                int color = -2184710;
+            case R.id.edit:
+
+                editMode();
+                actionMenu.findItem(R.id.edit).setVisible(false);
+                actionMenu.findItem(R.id.delete).setVisible(false);
+                actionMenu.findItem(R.id.save).setVisible(false);
+                actionMenu.findItem(R.id.update).setVisible(true);
+
+                return true;
+
+            case R.id.update:
+                //Update
 
                 if (title.isEmpty()) {
                     et_title.setError("Please enter a title");
                 } else if (note.isEmpty()) {
                     et_note.setError("Please enter a note");
                 } else {
-                    saveNote(title, note, color);
+                    presenter.updateNote(id, title, note, color);
                 }
+
+                return true;
+
+            case R.id.delete:
+
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+                alertDialog.setTitle("Confirm !");
+                alertDialog.setMessage("Are you sure?");
+                alertDialog.setNegativeButton("Yes", (dialog, which) -> {
+                    dialog.dismiss();
+                    presenter.deleteNote(id);
+                });
+                alertDialog.setPositiveButton("Cencel",
+                        (dialog, which) -> dialog.dismiss());
+
+                alertDialog.show();
+
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    private void saveNote(final String title, final String note, final int color) {
 
+    @Override
+    public void showProgress() {
         progressDialog.show();
+    }
 
-        apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
-        Call<Note> call = apiInterface.saveNote(title, note, color);
+    @Override
+    public void hideProgress() {
+        progressDialog.hide();
+    }
 
-        call.enqueue(new Callback<Note>() {
-            @Override
-            public void onResponse(@NonNull Call<Note> call, @NonNull Response<Note> response) {
-                progressDialog.dismiss();
+    @Override
+    public void onRequestSuccess(String message) {
+        Toast.makeText(EditorActivity.this,
+                message,
+                Toast.LENGTH_SHORT).show();
+        setResult(RESULT_OK);
+        finish(); //back to main activity
+    }
 
-                if (response.isSuccessful() && response.body() != null) {
+    @Override
+    public void onRequestError(String message) {
+        Toast.makeText(EditorActivity.this,
+                message,
+                Toast.LENGTH_SHORT).show();
+    }
 
-                    Boolean success = response.body().getSuccess();
-                    if (success) {
-                        Toast.makeText(EditorActivity.this,
-                                response.body().getMessage(),
-                                Toast.LENGTH_SHORT).show();
-                        finish(); //back to main activity
-                    } else {
-                        Toast.makeText(EditorActivity.this,
-                                response.body().getMessage(),
-                                Toast.LENGTH_SHORT).show();
-                        // if error, still in this act.
-                    }
+    private void setDataFromIntentExtra() {
 
-                }
+        if (id != 0) {
+            et_title.setText(title);
+            et_note.setText(note);
+            palette.setSelectedColor(color);
 
-            }
+            getSupportActionBar().setTitle("Update Note");
+            readMode();
+        } else {
+            palette.setSelectedColor(getResources().getColor(R.color.white));
+            color = getResources().getColor(R.color.white);
+            editMode();
+        }
 
-            @Override
-            public  void  onFailure(@NonNull Call<Note> call, @NonNull Throwable t) {
-                progressDialog.dismiss();
-                Toast.makeText(EditorActivity.this,
-                        t.getLocalizedMessage(),
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
+    }
+
+    private void editMode() {
+        et_title.setFocusableInTouchMode(true);
+        et_note.setFocusableInTouchMode(true);
+        palette.setEnabled(true);
+    }
+
+    private void readMode() {
+        et_title.setFocusableInTouchMode(false);
+        et_note.setFocusableInTouchMode(false);
+        et_title.setFocusable(false);
+        et_note.setFocusable(false);
+        palette.setEnabled(false);
     }
 }
